15c93813899bac5574fbd06c52a70ead015d36ed
김현진 <hjclub123@gmail.com>
2023-12-11 22:51:26 +0900

    update

--- src/page/ProductAll.js
+++ src/page/ProductAll.js
@@ -1,18 +1,25 @@
 import React, {useEffect, useState} from "react";
 import ProductCard from "../component/ProductCard";
-import {Col, Container, Row} from "react-bootstrap";
+import {Alert, Col, Container, Row} from "react-bootstrap";
 import {useSearchParams} from "react-router-dom";
 
 const ProductAll = () => {
     const [products, setProducts] = useState([]);
     const [query, setQuery] = useSearchParams();
+    let [error, setError] = useState("");
 
     const getProducts = async () => {
-        let searchQuery = query.get("q") || "";
-        let url = `https://my-json-server.typicode.com/hjclub123/hnm-react-netlify/products?q=${searchQuery}`;
+        let keyword = query.get("q") || "";
+        let url = `https://my-json-server.typicode.com/hjclub123/hnm-react-netlify/products?q=${keyword}`;
         let response = await fetch(url);
         let data = await response.json();
-        console.log(data);
+        if (data.length < 1) {
+            if (keyword !== "") {
+                setError(`${keyword}와 일치하는 상품이 없습니다`);
+            } else {
+                throw new Error("결과가 없습니다");
+            }
+        }
         setProducts(data);
     }
 
@@ -22,12 +29,18 @@ const ProductAll = () => {
     useEffect(() => {
         getProducts().catch(error => {
             console.error(error);
+            setError(error.message);
         })
     }, [query]);
 
     return (
         <div>
             <Container> {/* 아이템이 가운데로 모이게 해 줌 */}
+                {error ? (
+                    <Alert variant="danger" className="text-center">
+                        {error}
+                    </Alert>
+                ) : (
                 <Row>
                     {products.length > 0 &&
                         products.map((product) => (
@@ -37,6 +50,7 @@ const ProductAll = () => {
                         </Col>
                     ))}
                 </Row>
+                )}
             </Container>
         </div>
     );
